39b700758010288b8400a7db05f8b4ceea649f59
tobias <tobias.omondi@student.moringaschool.com>
2024-07-12 09:25:07 +0300

    Grading Done

---
+++ componets/Ticket.js
@@ -0,0 +1,14 @@
+// const prompt = require('prompt-sync') ({sigint: true});
+
+// let age = parseFloat(prompt("ENTER YOUR AGE:"));
+
+// // coversion logic
+
+// let adult;
+// let children;
+
+// if (age >= 65 ) {
+//     adult = ("")
+// } else {
+    
+// }
