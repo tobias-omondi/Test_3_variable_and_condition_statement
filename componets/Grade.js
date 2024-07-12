bdd2ea19784ac3c0c08762dc9f8bbb4105ae381c
tobias <tobias.omondi@student.moringaschool.com>
2024-07-12 09:15:34 +0300

    Grading Done

--- componets/Grade.js
+++ componets/Grade.js
@@ -2,16 +2,23 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let grade = parseFloat (prompt("Enter the Grade:"));
 let result;
+let LetterGrade;
 
 if (grade >= 90) {
     result = ("Excellents");
+    LetterGrade = ("A");
 }else if ( grade >= 80) {
     result = ("Very Good");
+    LetterGrade = ("B");
 }else if (grade >= 70) {
     result = ("Well Done");
+    LetterGrade = ("C");
 }else if (grade >= 60) {
     result = ("Avarage");
+    LetterGrade = ("D")
 }else {
-    result = ("Work hard");
+    result = ("Work Hard");
+    LetterGrade = ("F");
 };
 console.log (result);
+console.log (`Your Grade is ${LetterGrade}`);
