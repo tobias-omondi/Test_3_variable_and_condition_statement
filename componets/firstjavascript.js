e2c2ef71eead2dadeb1d8c0a4c2f25390f22f55e
tobias <tobias.omondi@student.moringaschool.com>
2025-04-28 13:28:08 +0300

    varibles assignments

---
+++ componets/firstjavascript.js
@@ -0,0 +1,35 @@
+console.log("Variables and Operations")
+console.log("Hello, Tobias!")
+
+// VARIABLE 
+// This are containers ni kama box zenye zinaweka staff that can be used in future or zinakuwa stored kwa box nikiitaji nitaitumia.
+// example nikama siku yangu ya birthday imekuwa store during a specific date akuna date ingine so varible inajua date ndio nitacelbrate birthday so ime store my birth date.
+
+
+// LET VARIBLE CAN BE RE-ASSIGNING
+let name = "Tobias"
+let surname = "Ogola"
+
+console.log(name);
+console.log(surname);
+
+// You can also re-assign variables:
+
+let age = 11
+console.log(age)
+
+age = 22
+
+console.log (age)
+
+// one thing you need to know when are calling console.log tunaiita jina ya box ndio utupatiye nn iko dani yake we r not calling the value
+
+// CONST VARIBLE ARE CONSTANT CANT BE RE-ASSIGNING
+
+const pi = 0.33
+console.log(pi)
+
+pi = 0.78
+
+console.log(pi)  // this will throw an error in our code 
+
