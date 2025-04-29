92778d9d6c0e4d63d66cf7604b1d5e670767b763
tobias <tobias.omondi@student.moringaschool.com>
2025-04-29 22:05:32 +0300

    learned datatypes in javascripts

---
+++ componets/datatypes.js
@@ -0,0 +1,26 @@
+// DATA TYPES
+
+let name = "Tobias"
+console.log(name)
+
+name = 22.5
+console.log(name)
+
+ ~ Numbers
+
+let num1= 120
+console.log(num1)
+
+// arithemetics
+
+let num2 = 120
+let num3 = 220
+
+console.log(num2 + num3)
+
+~ Strings
+
+let school = "Our lady of Nazareth"
+let HighSchool = "Holy Ghost Schools Makueni"
+let Schoolss = `${"Tobias was a student at"} Thika road schools`
+console.log(Schoolss)
