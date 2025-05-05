514438ab560f6e0f51dd617464aa1238163fe854
tobias <tobias.omondi@student.moringaschool.com>
2025-05-05 10:53:46 +0300

    functions intro

--- componets/datatypes.js
+++ componets/datatypes.js
@@ -24,3 +24,31 @@ let school = "Our lady of Nazareth"
 let HighSchool = "Holy Ghost Schools Makueni"
 let Schoolss = `${"Tobias was a student at"} Thika road schools`
 console.log(Schoolss)
+
+~ Boolean //(Logical type)
+
+let nameFieldChecked = true; // yes, name field is checked
+let ageFieldChecked = false; // no, age field is not checked
+
+
+~ THE NULL VALUE
+
+let age = null;
+
+//In JavaScript, null is not a “reference to a non-existing object” or a “null pointer” like in some other languages.
+
+// It’s just a special value which represents “nothing”, “empty” or “value unknown”.
+
+// The code above states that age is unknown.
+
+
+~ THE undefined VALUE
+
+let age = 100;
+
+// change the value to undefined
+age = undefined;
+
+alert(age); // "undefined"
+
+~ Objects and Symbols
